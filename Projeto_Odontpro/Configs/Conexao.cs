b6a607eade0a3290cf9b068f9a5690bf0a206e29
Douglinhas <douglas.jipa140@gmail.com>
2025-10-09 17:20:53 -0400

    Estoque
    Incluimos estoque e seus códigos

--- Projeto_Odontpro/Configs/Conexao.cs
+++ Projeto_Odontpro/Configs/Conexao.cs
@@ -1,7 +1,27 @@
-﻿using MySql.Data;
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
 namespace Projeto_Odontpro.Configs
 {
     public class Conexao
     {
+        private readonly string _connectionString;
+        public Conexao(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MySqlConnection") ?? "";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            var conn = new MySqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+
+        public MySqlCommand CreateCommand(string query, MySqlConnection? conn = null)
+        {
+            conn ??= GetConnection();
+            return new MySqlCommand(query, conn);
+        }
     }
 }
---
+++ Projeto_Odontpro/Models/Estoque.cs
@@ -0,0 +1,13 @@
+﻿namespace Projeto_Odontpro.Models
+{
+    public class Estoque
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Quantidade { get; set; }
+        public string? FormaPagamento { get; set; }
+        public string? Marca { get; set; }
+        public decimal Valor { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
