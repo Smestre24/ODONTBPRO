a62be0beeb8e20252252ef6f90442b88de1cbeac
Maria <mariaisabelijipaa@gmail.com>
2025-11-21 12:24:06 -0400

    teste

--- Projeto_Odontpro/Models/ContasAPagar.cs
+++ Projeto_Odontpro/Models/ContasAPagar.cs
@@ -2,6 +2,7 @@
 {
     public class ContasAPagar
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
