27885144d0f6ba0acb0bea8382c2caf389e38da4
Maria <mariaisabelijipaa@gmail.com>
2025-11-25 16:15:05 -0400

    paciente
    ana carolina

--- Projeto_Odontpro/Models/Paciente/PacienteDAO.cs
+++ Projeto_Odontpro/Models/Paciente/PacienteDAO.cs
@@ -1,6 +1,5 @@
 using Projeto_Odontpro.Configs;
 
-
 namespace Projeto_Odontpro.Models.Paciente
 {
     public class PacienteDAO
@@ -24,11 +23,10 @@ namespace Projeto_Odontpro.Models.Paciente
                 var paciente = new Paciente();
                 paciente.Id = leitor.GetInt32("id_pac");
                 paciente.Nome = DAOHelper.GetString(leitor, "nome_pac");
-                paciente.Sexo = DAOHelper.GetString(leitor, "sexo_pac");
                 paciente.Telefone = DAOHelper.GetString(leitor, "telefone_pac");
-                paciente.Nascimento = leitor.GetDateTime("data_nascimento_pac");
-                paciente.Estado = DAOHelper.GetString(leitor, "estado_pac");
-                paciente.Observacoes = DAOHelper.GetString(leitor, "observacoes_pac");
+                paciente.Email = DAOHelper.GetString(leitor, "email_pac");
+                paciente.Endereco = DAOHelper.GetString(leitor, "endereco_pac");
+                paciente.Nascimento = leitor.GetDateTime("nascimento_pac");
 
                 lista.Add(paciente);
             }
@@ -40,27 +38,90 @@ namespace Projeto_Odontpro.Models.Paciente
         {
             try
             {
-                var comando = _conexao.CreateCommand(
-                    "INSERT INTO Paciente VALUES (null, @_nome, @_sexo, @_telefone, @_dataNascimento, @_estado, @_observacoes)");
+                var sql = @"INSERT INTO paciente  (nome_pac, telefone_pac, email_pac, endereco_pac, nascimento_pac) 
+             VALUES (@nome, @telefone, @email, @endereco, @nascimento);";
 
-                comando.Parameters.AddWithValue("@_nome", paciente.Nome);
-                comando.Parameters.AddWithValue("@_sexo", paciente.Sexo);
-                comando.Parameters.AddWithValue("@_telefone", paciente.Telefone);
-                comando.Parameters.AddWithValue("@_dataNascimento", paciente.Nascimento);
-                comando.Parameters.AddWithValue("@_estado", paciente.Estado);
-                comando.Parameters.AddWithValue("@_observacoes", paciente.Observacoes);
+                var cmd = _conexao.CreateCommand(sql);
+
+                cmd.Parameters.AddWithValue("@nome", paciente.Nome);
+                cmd.Parameters.AddWithValue("@telefone", paciente.Telefone);
+                cmd.Parameters.AddWithValue("@email", paciente.Email);
+                cmd.Parameters.AddWithValue("@endereco", paciente.Endereco);
+                cmd.Parameters.AddWithValue("@nascimento", paciente.Nascimento);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public Paciente? BuscarPorId(int id)
+        {
+            var comando = _conexao.CreateCommand(
+                "SELECT * FROM paciente WHERE id_pac = @id;");
+            comando.Parameters.AddWithValue("@id", id);
+
+            var leitor = comando.ExecuteReader();
+
+            if (leitor.Read())
+            {
+                var paciente = new Paciente();
+                paciente.Id = leitor.GetInt32("id_pac");
+                paciente.Nome = DAOHelper.GetString(leitor, "nome_pac");
+                paciente.Telefone = DAOHelper.GetString(leitor, "telefone_pac");
+                paciente.Email = DAOHelper.GetString(leitor, "email_pac");
+                paciente.Endereco = DAOHelper.GetString(leitor, "endereco_pac");
+                paciente.Nascimento = leitor.GetDateTime("nascimento_pac");
+
+                return paciente;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Atualizar(Paciente paciente)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                    "UPDATE paciente SET nome_pac = @nome, telefone_pac = @telefone, " +
+                    "email_pac = @email, endereco_pac = @endereco, nascimento_pac = @nascimento " +
+                    "WHERE id_pac = @id;");
+
+                comando.Parameters.AddWithValue("@nome", paciente.Nome);
+                comando.Parameters.AddWithValue("@telefone", paciente.Telefone);
+                comando.Parameters.AddWithValue("@email", paciente.Email);
+                comando.Parameters.AddWithValue("@endereco", paciente.Endereco);
+                comando.Parameters.AddWithValue("@nascimento", paciente.Nascimento);
+                comando.Parameters.AddWithValue("@id", paciente.Id);
 
                 comando.ExecuteNonQuery();
             }
-            catch (Exception)
+            catch
             {
                 throw;
             }
         }
+
         public void Excluir(int id)
         {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                    "DELETE FROM paciente WHERE id_pac = @id;");
 
+                comando.Parameters.AddWithValue("@id", id);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
         }
-    
     }
 }
